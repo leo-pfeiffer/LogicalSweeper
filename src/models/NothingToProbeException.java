4b1017f8bba36e866001d7ee3551eec6bb589ec4
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-02-18 08:16:14 +0000

    feat: add NothingToProbeException that is thrown when an agent has no more cells to probe

---
+++ src/models/NothingToProbeException.java
@@ -0,0 +1,10 @@
+package models;
+
+/**
+ * Exception that is thrown when an agent has no more cells to probe.
+ * */
+public class NothingToProbeException extends RuntimeException{
+    public NothingToProbeException(String message){
+        super(message);
+    }
+}
