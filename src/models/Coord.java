22f5e725af08e141077ccad9018481c764d922a6
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-02-18 08:43:35 +0000

    feat: implement equals method in Coord

--- src/models/Coord.java
+++ src/models/Coord.java
@@ -1,11 +1,16 @@
 package models;
 
+import java.util.Objects;
+
 public class Coord {
 
     private final int row;
     private final int col;
 
     public Coord(int row, int col) {
+        if (row < 0 || col < 0) {
+            throw new IllegalArgumentException("Coordinates must be positive");
+        }
         this.row = row;
         this.col = col;
     }
@@ -16,5 +21,18 @@ public class Coord {
 
     public int getCol() {
         return col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coord coord = (Coord) o;
+        return row == coord.row && col == coord.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 }
