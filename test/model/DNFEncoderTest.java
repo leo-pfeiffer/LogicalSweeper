f101b165143d34b4dccab1b14d1a81d1bb47f1cd
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-02-22 19:05:24 +0000

    test: setup DNFEncoderTest

---
+++ test/model/DNFEncoderTest.java
@@ -0,0 +1,17 @@
+package model;
+
+import model.logic.DNFEncoder;
+import org.junit.Test;
+import org.logicng.formulas.Formula;
+
+public class DNFEncoderTest {
+
+    @Test
+    public void testSetup() {
+        DNFEncoder dnf = new DNFEncoder();
+        String[] cells = {"D1", "D2", "D3", "D4"};
+        Formula formula = dnf.encode(cells, 2);
+        int x = 1;
+    }
+
+}
