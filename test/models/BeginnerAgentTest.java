185d890740f37bdbe56b6bde3bc47ec2b693086e
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-02-22 10:58:59 +0000

    tests: update WorldTests, add BeginnerAgentTest

---
+++ test/models/BeginnerAgentTest.java
@@ -0,0 +1,96 @@
+package models;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import delegate.Game;
+import org.junit.Test;
+
+public class BeginnerAgentTest {
+
+    String agentName = "P2";
+
+    @Test
+    public void testSetup() {
+        Game game = new Game(World.TEST1, agentName);
+        new BeginnerAgent(game, World.TEST1.createNewView());
+    }
+
+    /**
+     * Agent wins.
+     * */
+    @Test
+    public void testWorld1() {
+        Game game = new Game(World.TEST1, agentName);
+        new BeginnerAgent(game, World.TEST1.createNewView());
+        game.run();
+        assertFalse(game.agentHasDied());
+        assertTrue(game.agentHasWon());
+        assertFalse(game.isPlaying());
+    }
+
+    /**
+     * Agent wins.
+     * */
+    @Test
+    public void testWorld2() {
+        Game game = new Game(World.TEST2, agentName);
+        new BeginnerAgent(game, World.TEST2.createNewView());
+        game.run();
+        assertFalse(game.agentHasDied());
+        assertTrue(game.agentHasWon());
+        assertFalse(game.isPlaying());
+    }
+
+    /**
+     * Agent doesn't terminate.
+     * */
+    @Test
+    public void testWorld3() {
+        Game game = new Game(World.TEST3, agentName);
+        new BeginnerAgent(game, World.TEST3.createNewView());
+        game.run();
+        assertFalse(game.agentHasDied());
+        assertFalse(game.agentHasWon());
+        assertFalse(game.isPlaying());
+    }
+
+    /**
+     * Agent doesn't terminate.
+     * */
+    @Test
+    public void testWorld4() {
+        Game game = new Game(World.TEST4, agentName);
+        new BeginnerAgent(game, World.TEST4.createNewView());
+        game.run();
+        assertFalse(game.agentHasDied());
+        assertFalse(game.agentHasWon());
+        assertFalse(game.isPlaying());
+    }
+
+    /**
+     * Agent doesn't terminate.
+     * */
+    @Test
+    public void testWorld5() {
+        Game game = new Game(World.TEST5, agentName);
+        new BeginnerAgent(game, World.TEST5.createNewView());
+        game.run();
+        assertFalse(game.agentHasDied());
+        assertFalse(game.agentHasWon());
+        assertFalse(game.isPlaying());
+    }
+
+    /**
+     * Agent doesn't terminate.
+     * */
+    @Test
+    public void testWorld6() {
+        Game game = new Game(World.TEST6, agentName);
+        new BeginnerAgent(game, World.TEST6.createNewView());
+        game.run();
+        assertFalse(game.agentHasDied());
+        assertFalse(game.agentHasWon());
+        assertFalse(game.isPlaying());
+    }
+}
--- test/models/WorldTest.java
+++ test/models/WorldTest.java
@@ -14,38 +14,36 @@ public class WorldTest {
     @Test
     public void testIsInWorld() {
         World world = World.TEST1;
-        assertTrue(world.isInWorld(new Coord(0, 0)));
-        assertTrue(world.isInWorld(new Coord(0, 2)));
-        assertTrue(world.isInWorld(new Coord(1, 1)));
-        assertTrue(world.isInWorld(new Coord(2, 0)));
-        assertTrue(world.isInWorld(new Coord(2, 2)));
+        assertTrue(world.containsCoord(new Coord(0, 0)));
+        assertTrue(world.containsCoord(new Coord(0, 2)));
+        assertTrue(world.containsCoord(new Coord(1, 1)));
+        assertTrue(world.containsCoord(new Coord(2, 0)));
+        assertTrue(world.containsCoord(new Coord(2, 2)));
 
-        assertFalse(world.isInWorld(new Coord(0, 3)));
-        assertFalse(world.isInWorld(new Coord(3, 0)));
-        assertFalse(world.isInWorld(new Coord(3, 3)));
+        assertFalse(world.containsCoord(new Coord(0, 3)));
+        assertFalse(world.containsCoord(new Coord(3, 0)));
+        assertFalse(world.containsCoord(new Coord(3, 3)));
 
-        assertThrows(IllegalArgumentException.class, () -> world.isInWorld(new Coord(-1, 0)));
-        assertThrows(IllegalArgumentException.class, () -> world.isInWorld(new Coord(0, -1)));
-        assertThrows(IllegalArgumentException.class, () -> world.isInWorld(new Coord(-1, -1)));
+        assertThrows(IllegalArgumentException.class, () -> world.containsCoord(new Coord(-1, 0)));
+        assertThrows(IllegalArgumentException.class, () -> world.containsCoord(new Coord(0, -1)));
+        assertThrows(IllegalArgumentException.class, () -> world.containsCoord(new Coord(-1, -1)));
     }
 
     @Test
     public void testGetAdjacentCoords() {
         World world = World.TEST1;
-        assertThrows(IllegalArgumentException.class, () -> world.isInWorld(new Coord(-1, -1)));
+        assertThrows(IllegalArgumentException.class, () -> world.containsCoord(new Coord(-1, -1)));
 
-        Coord[] coords1 = world.getAdjacentCoords(new Coord(0, 0));
-        ArrayList<Coord> coords1List = new ArrayList<>(Arrays.asList(coords1));
+        ArrayList<Coord> coords1List = world.getAdjacentCoords(new Coord(0, 0));
 
-        assertEquals(3, coords1.length);
+        assertEquals(3, coords1List.size());
         assertTrue(coords1List.contains(new Coord(0, 1)));
         assertTrue(coords1List.contains(new Coord(1, 1)));
         assertTrue(coords1List.contains(new Coord(1, 0)));
 
-        Coord[] coords2 = world.getAdjacentCoords(new Coord(1, 1));
-        ArrayList<Coord> coords2List = new ArrayList<>(Arrays.asList(coords2));
+        ArrayList<Coord> coords2List = world.getAdjacentCoords(new Coord(1, 1));
 
-        assertEquals(8, coords2.length);
+        assertEquals(8, coords2List.size());
         assertTrue(coords2List.contains(new Coord(0, 0)));
         assertTrue(coords2List.contains(new Coord(0, 1)));
         assertTrue(coords2List.contains(new Coord(0, 2)));
@@ -57,10 +55,9 @@ public class WorldTest {
         assertTrue(coords2List.contains(new Coord(2, 1)));
         assertTrue(coords2List.contains(new Coord(2, 2)));
 
-        Coord[] coords3 = world.getAdjacentCoords(new Coord(2, 2));
-        ArrayList<Coord> coords3List = new ArrayList<>(Arrays.asList(coords3));
+        ArrayList<Coord> coords3List = world.getAdjacentCoords(new Coord(2, 2));
 
-        assertEquals(3, coords3.length);
+        assertEquals(3, coords3List.size());
         assertTrue(coords3List.contains(new Coord(2, 1)));
         assertTrue(coords3List.contains(new Coord(1, 1)));
         assertTrue(coords3List.contains(new Coord(1, 2)));
