001eda7a5166fe0e019e64b21477f6cabfb31d2f
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-02-22 19:06:09 +0000

    feat: add EntailmentChecker

---
+++ src/model/logic/EntailmentChecker.java
@@ -0,0 +1,31 @@
+package model.logic;
+
+import org.logicng.formulas.Formula;
+import org.logicng.formulas.FormulaFactory;
+import org.logicng.formulas.Literal;
+import org.logicng.solvers.MiniSat;
+import org.logicng.solvers.SATSolver;
+
+public class EntailmentChecker {
+
+    private final KnowledgeBase kb;
+
+    public EntailmentChecker(KnowledgeBase kb) {
+        this.kb = kb;
+    }
+
+    public boolean checkEntailment(String literal, boolean phase) {
+        FormulaFactory f = kb.getF();
+        return checkEntailment(f.literal(literal, phase));
+    }
+
+    public boolean checkEntailment(Literal literal) {
+        FormulaFactory f = kb.getF();
+
+        Formula entailment = f.and(kb.getKBU(), literal);
+
+        SATSolver miniSat = MiniSat.miniSat(f);
+        miniSat.add(entailment);
+        return miniSat.sat().ordinal() == 0;
+    }
+}
