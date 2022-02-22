f43458fefeda09636b06ffd75e7f86ca0a6ec593
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-02-22 08:27:24 +0000

    feat: add GameBoardUtils class and CharMap interface

---
+++ src/models/CharMap.java
@@ -0,0 +1,30 @@
+package models;
+
+/**
+ * An interface for classes wrapping a 2d array of characters.
+ * */
+public interface CharMap {
+
+    /**
+     * Get the size of the map.
+     * */
+    int getSize();
+
+    /**
+     * Returns true if the given coord is in the map.
+     * @param coord The coordinate to check.
+     * */
+    boolean containsCoord(Coord coord);
+
+    /**
+     * Returns the value of the cell of the given coord from the map.
+     * @param coord The coordinate to get the value of.
+     * */
+    char getCell(Coord coord);
+
+    /**
+     * Return an array of cells adjacent to the given coordinate.
+     * @param coord The coordinate to get the adjacent cells of.
+     * */
+    Coord[] getAdjacentCoords(Coord coord);
+}
---
+++ src/models/GameBoardUtils.java
@@ -0,0 +1,79 @@
+package models;
+
+import java.util.ArrayList;
+
+/**
+ * Util class to provide functionalities for game boards.
+ * */
+public abstract class GameBoardUtils {
+
+    public static boolean containsCoord(Coord coord, int mapSize) {
+        int row = coord.getRow();
+        int col = coord.getCol();
+        return col >= 0 && col < mapSize && row >= 0 && row < mapSize;
+    }
+
+    public static char getCell(Coord coord, char[][] map) {
+        if (!GameBoardUtils.containsCoord(coord, map.length)) {
+            throw new IllegalArgumentException("Coordinates outside the map.");
+        }
+        return map[coord.getRow()][coord.getCol()];
+    }
+
+    public static Coord[] getAdjacentCoords(Coord coord, char[][] map) {
+
+        if (!GameBoardUtils.containsCoord(coord, map.length)) {
+            throw new IllegalArgumentException("Coordinates outside the map.");
+        }
+
+        ArrayList<Coord> adjacentCoords = new ArrayList<>();
+
+        int row = coord.getRow();
+        int col = coord.getCol();
+
+        for (int i = row - 1; i <= row + 1; i++) {
+            for (int j = col - 1; j <= col + 1; j++) {
+                if (i >= 0 && j >= 0 && (i != row || j != col)) {
+                    Coord adjCoord = new Coord(i, j);
+                    if (containsCoord(adjCoord, map.length)) {
+                        adjacentCoords.add(adjCoord);
+                    }
+                }
+            }
+        }
+        Coord[] adjacentCoordsArray = new Coord[adjacentCoords.size()];
+        adjacentCoords.toArray(adjacentCoordsArray);
+        return adjacentCoordsArray;
+    }
+
+    public static int countOccurrence(char token, ArrayList<Coord> cells, char[][] map) {
+        int count = 0;
+        for (Coord coord : cells) {
+            if (GameBoardUtils.getCell(coord, map) == token) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countOccurrence(char token, char[][] map) {
+        return GameBoardUtils.countOccurrence(token, GameBoardUtils.getAllCoords(map.length), map);
+    }
+
+    public static boolean cellIsToken(char token, Coord coord, char[][] map) {
+        if (!GameBoardUtils.containsCoord(coord, map.length)) {
+            throw new IllegalArgumentException("Coordinates outside the map.");
+        }
+        return map[coord.getRow()][coord.getCol()] == token;
+    }
+
+    public static ArrayList<Coord> getAllCoords(int size) {
+        ArrayList<Coord> coords = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                coords.add(new Coord(i, j));
+            }
+        }
+        return coords;
+    }
+}
