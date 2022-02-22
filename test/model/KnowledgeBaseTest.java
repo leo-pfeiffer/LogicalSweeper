d74b7e691107d00586e0f68072bd26c80eecffa0
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-02-22 19:05:57 +0000

    test: add KnowledgeBaseTest

---
+++ test/model/KnowledgeBaseTest.java
@@ -0,0 +1,16 @@
+package model;
+
+import model.board.View;
+import model.logic.KnowledgeBase;
+import org.junit.Test;
+
+public class KnowledgeBaseTest {
+
+    @Test
+    public void testSetup() {
+        char[][] board = {{'0', '1', '?'}, {'0', '1', '?'}, {'0', '1', '?'}};
+        View view = new View(board, 1);
+        KnowledgeBase kb = new KnowledgeBase(view);
+    }
+
+}
